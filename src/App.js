c724066d036f0272ef46736600e535b7bdeed591
kkk1k <googija9818@gmail.com>
2023-10-18 16:58:37 +0900

    coin calculator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import { useState, useEffect } from "react";
-function Hello() {
-  
-  useEffect(() => {
-    console.log("hello")
-    return () =>{
-      console.log("bye")
-    }
-  },[]);
-  return <h1>Hello</h1>;
 
-}
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [loading, setLoading] = useState(true);
+  const [coin, setCoin] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoin(json);
+        setLoading(false);
+      });
+  }, []);
+  const [value, setValue] = useState(0);
+  const onchange = (event) =>{
+    setValue(event.target.value)
+  }
+  const [money, getMoney] = useState(0);
+  const changeMoney = (e) => getMoney(e.target.value);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>The Coins!</h1>
+      {loading ? null : <h2>{coin.length}</h2>}
+
+      {loading ? (
+        <strong>Loading~</strong>
+      ) : (
+        <select onChange={changeMoney}>
+          {coin.map((item, index) => (
+            <option key={index} value={item.quotes.USD.price}>
+              {item.name},({item.symbol}): ${(item.quotes.USD.price)} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <div>
+        <input value = {value} onChange={onchange} text="number" placeholder="write how much"></input>
+      </div>
+      <hr />
+    
+      <h2>{value === 0 ? "0" : value/money}</h2>
     </div>
   );
 }
