9e7e644805f0605a2f0fdc2c05c9032e70c82e82
kkk1k <googija9818@gmail.com>
2023-10-19 16:40:56 +0900

    plus css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,20 @@
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
-function Movie({id, medium_cover_image, title, summary, genres }) {
+import styles from "./Movie.module.css";
+function Movie({id, medium_cover_image, title, summary, genres,year }) {
   return (
-    <div>
-      <img src={medium_cover_image} alt={title} />
-      <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
-      <p>{summary}</p>
-      <ul>
+    <div className={styles.movie}>
+      <img src={medium_cover_image} alt={title} className={styles.movie__img} />
+      <div>
+      <h2 className={styles.movie__title}><Link to={`/movie/${id}`}>{title}</Link></h2>
+      <h3 className={styles.movie__year}>{year}</h3>
+      <p>{summary.length < 235 ? summary : `${summary.slice(0,235)}` }</p>
+      <ul className={styles.movie__genres}>
         {genres.map((g) => (
           <li key={g}>{g}</li>
         ))}
       </ul>
+      </div>
     </div>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
-
+import styles from "./Home.module.css";
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movie, setMovie] = useState([]);
@@ -19,20 +19,24 @@ function Home() {
   }, []);
   console.log(movie);
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1>loading...</h1>
+        <div className={styles.loader}>
+        <span>loading...</span></div>
       ) : (
-        movie.map((movie) => (
+        <div className={styles.movies}>
+        {movie.map((movie) => (
           <Movie
             key={movie.id}
             id={movie.id}
+            year={movie.year}
             medium_cover_image={movie.medium_cover_image}
             title={movie.title}
             summary={movie.summary}
             genres={movie.genres}
           />
-        ))
+        ))}
+        </div>
       )}
     </div>
   );
