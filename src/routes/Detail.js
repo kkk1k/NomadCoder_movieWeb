dfec88f67a041442fff59e95187e960e9756480a
kkk1k <googija9818@gmail.com>
2023-10-19 15:32:01 +0900

    make movie site

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json)
+    setMovie([json.data.movie]);
+    setLoading(false);
+    console.log(movie);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      {loading ? (
+        <h1>loading....</h1>
+      ) : (
+        movie.map((movie) => (
+          <div>
+            <h1>{movie.title}</h1>
+            <img src={movie.medium_cover_image} alt={movie.title} />
+            <p>{movie.description_intro}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default Detail;
